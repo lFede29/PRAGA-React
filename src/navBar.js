f52b1853ad075b0bfd231b1fc8075eddaaee6558
Federico Gomez <fedegomez2001@hotmail.com.ar>
2022-11-09 20:45:29 -0300

    PreEntrega1-Gomez Federico

---
+++ src/navBar.js
@@ -0,0 +1,14 @@
+function navBar (){
+   return(
+    <nav className="browser">
+        <a className="brandBusiness">PRAGA</a>
+        <ul>
+            <li><a href="">HOME</a></li>
+            <li><a href="">CONTACTO</a></li>
+            <li><a href="">PRODUCOS</a></li>
+        </ul>
+    </nav>
+    ) ;
+}
+
+export default navBar;
